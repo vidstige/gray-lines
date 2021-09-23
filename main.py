e374a138c7abee9f2eb7d06c05010ffe06a7acfc
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-09-23 21:40:57 +0200

    draw many images

--- main.py
+++ main.py
@@ -8,29 +8,48 @@ from PIL import Image
 TAU = 2 * np.pi
 
 
+def clone_surface(surface: cairo.ImageSurface) -> cairo.ImageSurface:
+    return cairo.ImageSurface.create_for_data(
+        surface.get_data(),
+        surface.get_format(),
+        surface.get_width(),
+        surface.get_height(),
+        surface.get_stride())
+
+
 def asarray(surface: cairo.ImageSurface) -> np.ndarray:
     buffer = surface.get_data()
     return np.ndarray(
-        shape=(surface.get_width(),surface.get_height(), 4),
+        shape=(surface.get_height(), surface.get_width()),
         dtype=np.uint8,
         buffer=buffer)
+
+
+def cost(diff: np.ndarray) -> float:
+    return np.sum(diff.ravel()**2)
 
 
 def main():
     n = 6
     img = Image.open('woman.jpeg')
     size = np.array([img.width, img.height])
+    target = np.asarray(img).astype(np.float32)
+
     r = np.max(size)
     alpha = np.linspace(0, TAU, n, endpoint=False)
     nodes = r * np.vstack([np.cos(alpha), np.sin(alpha)]).T + 0.5 * size
 
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, img.width, img.height)
-    ctx = cairo.Context(surface)
+    accumulator = cairo.ImageSurface(cairo.FORMAT_A8, *size)
     for n0, n1 in itertools.combinations(nodes, 2):
+        surface = clone_surface(accumulator)
+        ctx = cairo.Context(surface)
+        ctx.set_line_width(2)
         ctx.move_to(*n0)
         ctx.line_to(*n1)
         ctx.stroke()
-    surface.write_to_png("output.png")
+        diff = asarray(surface).astype(np.float32) - target
+        print(cost(diff))
+
 
 if __name__ == "__main__":
     main()
